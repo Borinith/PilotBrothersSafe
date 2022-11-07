b0cd0ff9668a27a3490d24db41cd34ca1498ce94
Borinith <b.krasnov@mail.ru>
2022-11-08 00:21:23 +0300

    Add buttons

--- PilotBrothersSafe/App.xaml.cs
+++ PilotBrothersSafe/App.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows;
-
-namespace PilotBrothersSafe
+﻿namespace PilotBrothersSafe
 {
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
--- PilotBrothersSafe/MainWindow.xaml.cs
+++ PilotBrothersSafe/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ namespace PilotBrothersSafe
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
@@ -34,6 +34,70 @@ namespace PilotBrothersSafe
                 int.TryParse((StartMenu.FindName("SafeSize") as IntegerUpDown)?.Text, NumberStyles.Integer, CultureInfo.InvariantCulture, out var safeSize);
 
                 StartMenu.Children.Clear();
+
+                var buttons = CreateMatrixSafe(safeSize);
+
+                MatrixDraw(buttons);
+            }
+        }
+
+        /// <summary>
+        ///     Создаем матрицу NxN и случайно заполняем ее ― и |
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private Button[,] CreateMatrixSafe(int n)
+        {
+            var buttonMatrix = new Button[n, n];
+            const bool defaultPosition = true;
+
+            for (var i = 0; i < n; i++) //Row
+            {
+                for (var j = 0; j < n; j++) //Column
+                {
+                    buttonMatrix[i, j] = new Button
+                    {
+                        Content = SetButtonContent(defaultPosition),
+                        Tag = defaultPosition,
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        Margin = new Thickness(0.5)
+                    };
+
+                    buttonMatrix[i, j].Click += ChangeMatrixSafe;
+                }
+            }
+
+            return buttonMatrix;
+        }
+
+        private static string SetButtonContent(bool position)
+        {
+            return position ? "|" : "—";
+        }
+
+        private void ChangeMatrixSafe(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MatrixDraw(Button[,] buttons)
+        {
+            CommonWindow.Children.Add(Game);
+
+            for (var i = 0; i < buttons.GetLength(0); i++) //Row
+            {
+                Game.RowDefinitions.Add(new RowDefinition());
+                Game.ColumnDefinitions.Add(new ColumnDefinition());
+
+                for (var j = 0; j < buttons.GetLength(1); j++) //Column    
+                {
+                    var buttonWithPosition = buttons[i, j];
+
+                    Grid.SetRow(buttonWithPosition, i);
+                    Grid.SetColumn(buttonWithPosition, j);
+
+                    Game.Children.Add(buttonWithPosition);
+                }
             }
         }
     }
